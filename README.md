da72cdf218befbf7ef2ebc0371b69079563b4794
mentalhealthawhereness <38903439+mentalhealthawhereness@users.noreply.github.com>
2019-09-05 13:27:26 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# MH aWHEREness
+
+MH aWHEREness is the viewing and crowdmapping platform for the mental health awhereness project.
+
+This was initially based on Raphael Dumas' repository - [crowdmap-basic](https://github.com/radumas/crowdmap-basic).
+
+Updated with more plugins and functions to make crowdmapping of mental health services and facilities easier even for non-GIS/non-mapping volunteers.
+
+Platforms and Libraries Used:
+- [Leaflet Draw](https://github.com/Leaflet/Leaflet.draw)
+- [Carto](https://carto.com/)
+- [Github Pages](https://pages.github.com/)
+- [Mapbox](https://mapbox.com/)
+- [Leaflet OSM Geocoder](https://github.com/k4r573n/leaflet-control-osm-geocoder)
+- [Leaflet Locate](https://github.com/domoritz/leaflet-locatecontrol)
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://github.com/anditabinas/mhawhereness/blob/master/license/LICENSE)
---
+++ sql/insert_function.sql
@@ -0,0 +1,38 @@
+DROP FUNCTION IF EXISTS insert_crowd_mapping_data(text,text,text,text,text,text,text,text,text,text);
+--Assumes only one value being inserted
+
+CREATE OR REPLACE FUNCTION insert_crowd_mapping_data (
+    _geojson TEXT,
+    _address TEXT,
+    _name TEXT,
+    _doctor TEXT,
+    _contactnum TEXT,
+    _email TEXT,	
+    _fee TEXT, 
+    _website TEXT,
+    _hours TEXT,
+    _notes TEXT)    
+--Has to return something in order to be used in a "SELECT" statement
+RETURNS integer
+AS $$
+DECLARE 
+    _the_geom GEOMETRY;
+	--The name of your table in cartoDB
+	_the_table TEXT := 'crowdmap_basic';
+BEGIN
+    --Convert the GeoJSON to a geometry type for insertion. 
+    _the_geom := ST_SetSRID(ST_GeomFromGeoJSON(_geojson),4326); 
+	
+
+	--Executes the insert given the supplied geometry, description, and username, while protecting against SQL injection.
+    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, address, name, doctor, contactnum, email, fee, website, hours, notes)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+            ' USING _the_geom, _address, _name, _doctor, _contactnum, _email, _fee, _website, _hours, _notes;
+            
+    RETURN 1;
+END;
+$$
+LANGUAGE plpgsql SECURITY DEFINER ;
+
+--Grant access to the public user
+GRANT EXECUTE ON FUNCTION insert_crowd_mapping_data( text, text, text, text, text, text, text, text, text, text) TO publicuser;
